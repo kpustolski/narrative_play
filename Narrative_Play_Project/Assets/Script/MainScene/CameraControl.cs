610bd3a94118ff845c1532333011b98c8771206a
YIWEN DAI <daiyw1992@gmail.com>
2015-11-13 12:53:26 -0800

    add grid to the main scene and camera control

---
+++ Narrative_Play_Project/Assets/Script/MainScene/CameraControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	public GameObject target;
+	public float minFov = 3.5f;
+	public float maxFov = 7f;
+	public float sensitivity = 1f;
+	public float rotate_Speed = 6f;
+	public float moveX_Speed = 0.7f;
+	public float moveY_Speed = 0.7f;
+	
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+
+	void Start()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		// Zoom In & Out with t
+		float fov = Camera.main.fieldOfView;
+		fov += -Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.orthographicSize = fov;
+
+		// Rotate the camera 
+		if (Input.GetMouseButton(1))
+		{
+			float mousX = Input.GetAxis("Mouse X");//得到鼠标移动距离
+			transform.RotateAround(target.transform.position, new Vector3(0, mousX, 0), rotate_Speed);
+		}
+
+	
+	}
+
+	
+	
+}
---
+++ Narrative_Play_Project/Assets/Script/MainScene/Grid.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	private Color startcolor;
+	private Renderer rend;
+	private bool isFilled = false;
+	public GameObject SCubePrefab;
+
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<Renderer> ();
+		startcolor = rend.material.color;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	// highlight the cube 
+	void OnMouseEnter()
+	{
+		//renderer.material.color = Color.yellow;
+		rend.material.color = Color.yellow;
+	}
+	void OnMouseExit()
+	{
+		rend.material.color = startcolor;
+	}
+
+	// select grid cell and place the cube 
+	// TODO: generate cube according to the cube index in the right position 
+	void OnMouseDown(){
+		if (!isFilled) {
+			Debug.Log ("Place a cube!!");
+			GameObject CloneCube;
+			Vector3 cposition = gameObject.transform.position;
+			cposition.y += 4.0F;
+			CloneCube = Instantiate (SCubePrefab, cposition, transform.rotation) as GameObject;
+			isFilled = true;
+		} else {
+			Debug.Log ("Cube already existed!!");
+		}
+
+	}
+}
