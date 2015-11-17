f7ae7b9ee8820dab96df5361883af32f5d0b930b
songbr309 <songbr309@gmail.com>
2015-11-17 15:08:45 -0800

    mainmenu

---
+++ Narrative_Play_Project/Assets/mainmenu.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function StartGame () {
+
+    Application.LoadLevel("Game");
+}
+
+function ExitGame () {
+    Application.Quit();
+}
