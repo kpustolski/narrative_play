5cf2ef979eebea96fbfd8c248e0a504d38d6f55d
Katie Pustolski <pustolskik@gmail.com>
2015-11-16 20:01:47 -0800

    Text is working but is still buggy. Use W to switch block in small screen.

--- Narrative_Play_Project/Assets/Script/MainScene/TurnCube.cs
+++ Narrative_Play_Project/Assets/Script/MainScene/TurnCube.cs
@@ -13,6 +13,8 @@ public class TurnCube : MonoBehaviour {
     private string labelText;
     Vector3 wHitRot = new Vector3(180, 0, 0);
 	Vector3 sHitRot = new Vector3(-180, 0, 0);
+	private bool canRotateUp;
+	private bool canRotateS;
 
    // Vector3 humanSide;
     //Vector3 alienSide;
@@ -22,6 +24,9 @@ public class TurnCube : MonoBehaviour {
     {
         isRotated = false;
         showText = false;
+		labelText = alienText;
+		canRotateUp = true;
+
   //      humanSide = new Vector3(0.3515936f, 0, 0);
        // alienSide = new Vector3(359.8242f, 180, 180);
     }
@@ -29,19 +34,14 @@ public class TurnCube : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        print("camera position.x: "+ selectCam1.transform.position.x);
-        print("cube position.x: " + (transform.position.x));
-        if (transform.rotation.x >= 358f && transform.rotation.x <= 360f  && selectCam1.transform.position.x == transform.position.x)
-        {
-               print("Equal positions human");
-               labelText = humanText;
-         
-        }
-       if (transform.rotation.x <= 1f && selectCam1.transform.position.x == transform.position.x)
+        //print("camera position.x: "+ selectCam1.transform.position.x);
+        //print("cube position.x: " + (transform.position.x));
+
+		/*else if (transform.rotation.x >= 0.01f && transform.rotation.x <= 100f && selectCam1.transform.position.x == transform.position.x)
         {
                print("Equal positions alien");
                labelText = alienText;            
-        }
+        }*/
 
         if(labelText != "")
         {
@@ -52,18 +52,36 @@ public class TurnCube : MonoBehaviour {
             labelText = "";
             showText = false;
         }
+		if (selectCam1.transform.position.x == transform.position.x) {
+			if (Input.GetKeyDown ("w")) {
+				iTween.RotateAdd (gameObject, wHitRot, 1);
+				print ("rotatae");
+				print ("Equal positions human");
 
-        if (Input.GetKeyDown("w")) {
-            iTween.RotateAdd(gameObject, wHitRot, 1);
-            print("rotatae");
+				if (canRotateUp) {
+					labelText = humanText;
+					canRotateUp = false;
+				} else if (!canRotateUp) {
+					labelText = alienText;
+					canRotateUp = true;
+				}
+
+				
+			}
+
+			/*if (Input.GetKeyDown("s"))
+			{
+				iTween.RotateAdd(gameObject, sHitRot, 1);
+				print("rotatae");
+				labelText = alienText;
+				//isRotated = true;
+			}*/
+		} else {
+			labelText="";
+		}
             //isRotated = true;
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            iTween.RotateAdd(gameObject, sHitRot, 1);
-            print("rotatae");
-            //isRotated = true;
-        }
+        
+     
     }
 
     void OnGUI()
