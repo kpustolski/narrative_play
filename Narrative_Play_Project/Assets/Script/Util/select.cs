7a1dbd1f75ea14c15e5e3b6c37c6e322b7a7261d
YIWEN DAI <daiyw1992@gmail.com>
2015-11-30 17:26:15 -0800

    add art for node

---
+++ Narrative_Play_Project/Assets/Script/Util/select.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class select : MonoBehaviour {
+
+	public Color colorStart;
+	public Color colorEnd;
+	public float duration = 1.0F;
+	public Renderer rend;
+
+	void Start() {
+		rend = GetComponent<MeshRenderer>();
+	}
+
+	void OnMouseEnter() {
+		Debug.Log ("Change color");
+		rend.material.color = colorEnd;
+	}
+
+	void OnMouseExit(){
+		Debug.Log ("Change back");
+		rend.material.color = colorStart;
+	}
+}
